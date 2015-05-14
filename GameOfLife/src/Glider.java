50030b0718a74f35e28f48d2534e407278b24e9f
Arshia Surti <arshia@mit.edu>
2015-05-14 07:28:00 -0400

    Added glider

--- GameOfLife/src/Glider.java
+++ GameOfLife/src/Glider.java
@@ -1,17 +1,29 @@
 import java.awt.Color;
+import java.util.ArrayList;
 
 
 public class Glider extends Agent{
 
 	public Glider(Simulator sim, Grid g, boolean runOnce, double chance) {
-		super(sim, g, runOnce, chance, Color.GREEN, 4, 4);
-		// TODO Auto-generated constructor stub
+		super(sim, g, runOnce, chance, Color.GREEN, 5, 5);
+		this.topY=(int) Math.floor(Math.random()*this.g.maxY);
+		this.leftX=(int) Math.floor(Math.random()*this.g.maxX);
 	}
 
 	@Override
 	void update() {
-		// TODO Auto-generated method stub
-		
+		ArrayList<Integer> blackTiles = new ArrayList<Integer>();
+		blackTiles.add(7); blackTiles.add(13); blackTiles.add(16); blackTiles.add(17); blackTiles.add(18);
+		for (int i=0; i<25; i++) {
+			if (blackTiles.contains(i)) {
+				if (!this.buffer.get(i).getOnOff())
+					this.buffer.get(i).flip();
+			}
+			else {
+				if (this.buffer.get(i).getOnOff())
+					this.buffer.get(i).flip();
+			}
+		}
 	}
 
 }
